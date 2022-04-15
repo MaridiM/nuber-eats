28d22654a61e063750b0a7110c9c77a4d0b14f51
MaridiM <maridim92@gmail.com>
2022-04-15 19:00:45 +0300

    Set config database and Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
+import * as Joi from 'joi'
 import { ConfigModule } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql'
 import { TypeOrmModule } from '@nestjs/typeorm'
@@ -12,14 +13,22 @@ import { RestaurantsModule } from './restaurants/restaurants.module'
             envFilePath:
                 process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
             ignoreEnvFile: process.env.NODE_ENV === 'prod',
+            validationSchema: Joi.object({
+                NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+                DB_HOST: Joi.string().required(),
+                DB_PORT: Joi.number().required(),
+                DB_USERNAME: Joi.string().required(),
+                DB_PASSWORDS: Joi.string().required(),
+                DB_NAME: Joi.string().required(),
+            }),
         }),
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'postgres',
-            database: 'nuber-eats',
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORDS,
+            database: process.env.DB_NAME,
             synchronize: true,
             logging: true,
         }),
